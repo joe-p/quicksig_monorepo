c18cc9249de9df8f41a7b50f4a25836666c3b3eb
Joe Polny <joepolny@gmail.com>
2022-01-26 22:18:13 +0000

    refactor as namespace/class

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,134 +2,146 @@ import algosdk from 'algosdk'
 import crypto from 'crypto'
 import MyAlgoConnect from '@randlabs/myalgo-connect'
 
-interface QuickSigData {
-  metadata: {
-    algod?: {
-      send: boolean,
-      algodServer: string,
-      algodToken: string,
-      algodPort: string,
-    },
+namespace QuickSig {
+  interface AlgodData {
+    send: boolean,
+    algodServer: string,
+    algodToken: string,
+    algodPort: string,
+  }
 
-    auth?: {
-      isAuth: boolean,
-      user: string,
-      service: string,
-      description: string,
-    },
+  interface AuthData {
+    isAuth: boolean,
+    user: string,
+    service: string,
+    description: string,
+  }
 
-    post?: {
-      onSigned?: string,
-      onComplete?: string,
-    },
+  interface PostData {
+    onSigned?: string,
+    onComplete?: string,
+  }
 
+  interface MetaData {
+    algod?: AlgodData,
+    auth?: AuthData,
+    post?: PostData,
     b64Txn: string,
     sigAddress: string,
     userAddress: string
-  },
-
-  sig: string,
-  hash: string,
-}
-
-function generateTable (obj: Object) {
-  let tableString = '<table class="table table-striped">'
-  for (const [key, value] of Object.entries(obj)) {
-    tableString += '<tr>'
-    tableString += `<td>${key}</td>`
-    tableString += `<td>${value}</td>`.replace('[object Object]', '')
-    tableString += '</tr>'
-  }
-  tableString += '</table>'
-
-  return tableString
-}
-
-function verifySig (hash: string, sig: string) {
-  const hashBuffer = Buffer.from(hash, 'hex')
-  const sigBuffer = Buffer.from(sig, 'base64')
-
-  if (algosdk.verifyBytes(hashBuffer, sigBuffer, data.metadata.sigAddress)) {
-    document.getElementById('sig-verification').innerHTML = 'Verified!'
-    document.getElementById('sig-value').innerHTML = data.metadata.sigAddress
-    return true
-  } else {
-    document.getElementById('sig-verification').innerHTML = 'Verification FAILED!'
-    document.getElementById('sig-value').innerHTML = 'Unknown'
-    return false
-  }
-}
-
-function verifyHash (metadata: Object, hash: string) {
-  const pathHash = window.location.pathname.substring(1)
-  const realHash = crypto.createHash('sha256').update(JSON.stringify(data.metadata)).digest('hex')
-
-  document.getElementById('hash-value').innerHTML = realHash
-
-  if (pathHash === realHash) {
-    document.getElementById('hash-verification').innerHTML = 'Verified!'
-    return true
-  } else {
-    console.log(pathHash, realHash)
-    return false
-  }
-}
-
-async function postData (url: string, data: any) {
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data)
-  })
-
-  return response.json()
-}
-
-function txnSigned (signedTxn: Uint8Array) {
-  const post = data.metadata.post
-
-  const b64SignedTxn = Buffer.from(signedTxn).toString('base64')
-
-  if (post && post.onSigned) {
-    postData(post.onSigned, { b64SignedTxn: b64SignedTxn }).then(data => {
-      console.log(data)
-    })
-  }
-}
-
-async function connectMyAlgo () {
-  const settings = {
-    shouldSelectOneAccount: true,
-    openManager: false
   }
 
-  // TODO: verify address connected is address in metadata
-  await myAlgoConnect.connect(settings)
-}
+  interface Data {
+    metadata: MetaData,
+    sig: string,
+    hash: string
+  }
 
-async function signMyAlgo () {
-  const signedTxn = (await myAlgoConnect.signTransaction(txn.toByte())).blob
+  export class ClientSession {
+    private myAlgoConnect: MyAlgoConnect
+    private data: Data
+    private txn: algosdk.Transaction
 
-  txnSigned(signedTxn)
-}
+    constructor (rawData: string) {
+      this.myAlgoConnect = new MyAlgoConnect()
 
-function init () {
-  document.getElementById('txn').innerHTML = generateTable(JSON.parse(txn.toString()))
+      this.data = JSON.parse(rawData)
+      this.txn = algosdk.decodeUnsignedTransaction(Buffer.from(this.data.metadata.b64Txn, 'base64'))
 
-  if (verifySig(data.hash, data.sig) && verifyHash(data.metadata, data.hash)) {
-    const myAlgoConnectBtn = document.getElementById('my-algo-connect') as HTMLButtonElement
-    myAlgoConnectBtn.addEventListener('click', connectMyAlgo)
-    myAlgoConnectBtn.disabled = false
+      document.getElementById('txn').innerHTML = this.generateTable(JSON.parse(this.txn.toString()))
 
-    const myAlgoSignBtn = document.getElementById('my-algo-sign') as HTMLButtonElement
-    myAlgoSignBtn.addEventListener('click', signMyAlgo)
-    myAlgoSignBtn.disabled = false
+      if (this.verifySig(this.data.hash, this.data.sig) && this.verifyHash(this.data.metadata, this.data.hash)) {
+        const myAlgoConnectBtn = document.getElementById('my-algo-connect') as HTMLButtonElement
+        myAlgoConnectBtn.addEventListener('click', () => { this.connectMyAlgo() })
+        myAlgoConnectBtn.disabled = false
+
+        const myAlgoSignBtn = document.getElementById('my-algo-sign') as HTMLButtonElement
+        myAlgoSignBtn.addEventListener('click', () => { this.signMyAlgo() })
+        myAlgoSignBtn.disabled = false
+      }
+    }
+
+    generateTable (obj: Object) {
+      let tableString = '<table class="table table-striped">'
+      for (const [key, value] of Object.entries(obj)) {
+        tableString += '<tr>'
+        tableString += `<td>${key}</td>`
+        tableString += `<td>${value}</td>`.replace('[object Object]', '')
+        tableString += '</tr>'
+      }
+      tableString += '</table>'
+
+      return tableString
+    }
+
+    verifySig (hash: string, sig: string) {
+      const hashBuffer = Buffer.from(hash, 'hex')
+      const sigBuffer = Buffer.from(sig, 'base64')
+
+      if (algosdk.verifyBytes(hashBuffer, sigBuffer, this.data.metadata.sigAddress)) {
+        document.getElementById('sig-verification').innerHTML = 'Verified!'
+        document.getElementById('sig-value').innerHTML = this.data.metadata.sigAddress
+        return true
+      } else {
+        document.getElementById('sig-verification').innerHTML = 'Verification FAILED!'
+        document.getElementById('sig-value').innerHTML = 'Unknown'
+        return false
+      }
+    }
+
+    verifyHash (metadata: Object, hash: string) {
+      const pathHash = window.location.pathname.substring(1)
+      const realHash = crypto.createHash('sha256').update(JSON.stringify(this.data.metadata)).digest('hex')
+
+      document.getElementById('hash-value').innerHTML = realHash
+
+      if (pathHash === realHash) {
+        document.getElementById('hash-verification').innerHTML = 'Verified!'
+        return true
+      } else {
+        console.log(pathHash, realHash)
+        return false
+      }
+    }
+
+    async postData (url: string, data: any) {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+      })
+
+      return response.json()
+    }
+
+    txnSigned (signedTxn: Uint8Array) {
+      const post = this.data.metadata.post
+
+      const b64SignedTxn = Buffer.from(signedTxn).toString('base64')
+
+      if (post && post.onSigned) {
+        this.postData(post.onSigned, { b64SignedTxn: b64SignedTxn }).then(data => {
+          console.log(data)
+        })
+      }
+    }
+
+    async connectMyAlgo () {
+      const settings = {
+        shouldSelectOneAccount: true,
+        openManager: false
+      }
+
+      // TODO: verify address connected is address in metadata
+      await this.myAlgoConnect.connect(settings)
+    }
+
+    async signMyAlgo () {
+      const signedTxn = (await this.myAlgoConnect.signTransaction(this.txn.toByte())).blob
+
+      this.txnSigned(signedTxn)
+    }
   }
 }
 
-const myAlgoConnect = new MyAlgoConnect()
-
-const rawData = document.getElementById('raw').innerHTML
-const data = JSON.parse(rawData) as QuickSigData
-const txn = algosdk.decodeUnsignedTransaction(Buffer.from(data.metadata.b64Txn, 'base64'))
-
-init()
+// eslint-disable-next-line no-new
+new QuickSig.ClientSession(document.getElementById('raw').innerHTML)
